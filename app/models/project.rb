388d8e0aaf162777d3ad2f1e65057968fc7aa1c1
Nate McGregor <natecmcgregor@gmail.com>
2017-12-01 12:25:58 -0800

    add project validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+  has_many :tags
+  validates :project, :name, :presence => true
+  validates :project, :date, :presence => true
+  validates :project, :description, :presence => true
+  validates :project, :image, :presence => true
 end
